db99dcc81b920d335f383b48fda6e4ce1c7b94fd
lchenevi <leo.cheneviere@gmail.com>
2024-04-23 15:09:46 +0200

    pushing task 3 again, added ANY

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Async waits for a random delay between 0 and max_delay seconds."""
 import asyncio
-from typing import Coroutine
+from typing import Any
+from asyncio import Task
 
 
 async def wait_random(max_delay: int = 10) -> float:
@@ -11,6 +12,6 @@ async def wait_random(max_delay: int = 10) -> float:
     return delay
 
 
-def task_wait_random(max_delay: int) -> asyncio.Task:
+def task_wait_random(max_delay: int) -> Task[Any]:
     """Creates and returns an asyncio.Task for the wait_random coroutine."""
     return asyncio.create_task(wait_random(max_delay))
