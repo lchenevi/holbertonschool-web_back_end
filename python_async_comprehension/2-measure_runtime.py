da6ce4682c9f7f200758745f8a269fde88494108
lchenevi <leo.cheneviere@gmail.com>
2024-04-25 10:49:44 +0200

    pushing task 3

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,33 @@
 #!/usr/bin/env python3
+"""Collect 10random nums using async comprehension over async_generator."""
+import asyncio
+from typing import List
+from time import perf_counter
+
+
+async def async_comprehension() -> List[float]:
+    """Collect 10random nums using async comprehension over async_generator."""
+    return [x async for x in async_generator()]
+
+
+async def measure_runtime() -> float:
+    """Executes async_comprehension"""
+    start_time = perf_counter()
+
+    # Execute async_comprehension four times in parallel using asyncio.gather
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+
+    end_time = perf_counter()
+    total_runtime = end_time - start_time
+    return total_runtime
+
+
+async def main():
+    return await measure_runtime()
+
+print(asyncio.run(main()))
