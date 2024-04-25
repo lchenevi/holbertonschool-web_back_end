064feb1f032f66d0fe4721920cc26fc3cc46d24b
lchenevi <leo.cheneviere@gmail.com>
2024-04-25 10:56:50 +0200

    pushing task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,12 @@
 """Asynchronously generates random numbers."""
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator() -> float:
+async def async_generator() -> AsyncGenerator[float, None]:
     """Asynchronously generates random numbers."""
-    for _ in range(10):
+
+    for _ in range(0, 10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
