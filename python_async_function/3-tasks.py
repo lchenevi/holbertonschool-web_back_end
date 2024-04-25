1ad458f435042869d4326f2b48ac362b768e17a1
lchenevi <leo.cheneviere@gmail.com>
2024-04-25 10:06:49 +0200

    pushing task 3

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python3
 """Async waits for a random delay between 0 and max_delay seconds."""
 import asyncio
-from typing import Any
-from asyncio import Task
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_random(max_delay: int = 10) -> float:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """Async waits for a random delay between 0 and max_delay seconds."""
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
-
-
-def task_wait_random(max_delay: int) -> Task[Any]:
-    """Creates and returns an asyncio.Task for the wait_random coroutine."""
     return asyncio.create_task(wait_random(max_delay))
