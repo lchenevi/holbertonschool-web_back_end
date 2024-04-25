b37a2b72f7667fc30f4b5cdb9f59554e22aaa69b
lchenevi <leo.cheneviere@gmail.com>
2024-04-25 10:51:26 +0200

    pushing task 3

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,33 +1,15 @@
 #!/usr/bin/env python3
 """Collect 10random nums using async comprehension over async_generator."""
+import time
 import asyncio
-from typing import List
-from time import perf_counter
-
-
-async def async_comprehension() -> List[float]:
-    """Collect 10random nums using async comprehension over async_generator."""
-    return [x async for x in async_generator()]
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Executes async_comprehension"""
-    start_time = perf_counter()
-
-    # Execute async_comprehension four times in parallel using asyncio.gather
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
-
-    end_time = perf_counter()
-    total_runtime = end_time - start_time
-    return total_runtime
-
-
-async def main():
-    return await measure_runtime()
-
-print(asyncio.run(main()))
+    """Collect 10random nums using async comprehension over async_generator."""
+    tic = time.time()
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    tac = time.time()
+    return (tac - tic)
+    # On obtient un temps d'exécution d'environ 10 secondes car les 4 appels
+    # sont exécutés en parallèle et chacun prend environ 10 x 1 secondes.
