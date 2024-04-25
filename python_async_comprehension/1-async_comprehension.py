ff489c7420efab70da7b85d2720199ff80e8bed4
lchenevi <leo.cheneviere@gmail.com>
2024-04-25 10:29:54 +0200

    pushing task 2

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""Import async_generator and write a coroutine that takes no arguments."""
+import asyncio
+from typing import List
+from random import uniform
+
+
+async def async_generator() -> float:
+    """Asynchronously generates random numbers."""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield uniform(0, 10)
+
+
+async def async_comprehension() -> List[float]:
+    """Collects 10random num using async comprehension over async_generator."""
+    return [x async for x in async_generator()]
