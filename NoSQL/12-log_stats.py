6aeb6013dc7a63ee1f64bb0ea0c886035cfb5d93
lchenevi <leo.cheneviere@gmail.com>
2024-04-30 13:22:01 +0200

    updating task 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,23 +2,24 @@
 """
 returns the list of school having a specific topic
 """
+
 from pymongo import MongoClient
 
 
-def log_stats():
-    """returns the list of school having a specific topic"""
-    client = MongoClient("mongodb://localhost:27017/")
-    logs = client.logs.nginx.count_documents({})
-    print(f"{logs} logs")
-    print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = client.logs.nginx.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
-    status = client.logs.nginx.count_documents({"method": "GET", "path":
-                                                "/status"})
-    print(f"{status} status check")
-
-
 if __name__ == "__main__":
-    log_stats()
+    """Provides stats about Nginx logs"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client['logs']
+    collection = db['nginx']
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    print(f"{collection.count_documents({})} logs")
+    print("Methods:")
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    print(collection.count_documents({"method": "GET", "path": "/status"}),
+          "status check")
+
+    client.close()
