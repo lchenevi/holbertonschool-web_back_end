7288913d1f384ebad27728b11c5c78d4a57eabb7
lchenevi <leo.cheneviere@gmail.com>
2024-05-08 17:41:45 +0200

    tiny indentation issue

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 export default class Building {
   constructor(sqft) {
     if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') { throw new Error('Class extending Building must override evacuationWarningMessage'); }
-    this.sqft = sqft;
+      this.sqft = sqft;
   }
 
   get sqft() {return this._sqft;}
