eedcf3ae19dce8a47b5a411e40e7396257ce7e66
lchenevi <leo.cheneviere@gmail.com>
2024-05-09 18:44:17 +0200

    tiny indentation issue

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -4,14 +4,23 @@ export default class Currency {
     this.name = name;
   }
 
-  get name() {return this._name;}
+  get name() {
+    return this._name;
+  }
 
-  set name(name) {this._name = name;}
+  set name(name) {
+    this._name = name;
+  }
 
-  get code() {return this._code;}
+  get code() {
+    return this._code;
+  }
 
-  set code(code) {this._code = code;}
+  set code(code) {
+    this._code = code;
+  }
 
-  displayFullCurrency() {return `${this._name} (${this._code})`;
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
   }
 }
