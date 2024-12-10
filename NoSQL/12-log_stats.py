0d669c3e2fdc11c517136876177bbad854639ce9
lchenevi <leo.cheneviere@gmail.com>
2024-12-10 11:09:11 +0100

    commenting my code

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python3
-"""Provides some stats about Nginx logs stored in MongoDB"""
+"""statitistiques sur les logs nginx"""
+
 
 from pymongo import MongoClient
 
+
 if __name__ == "__main__":
-    """Provides stats about Nginx logs"""
+    """Fournit des statistiques sur les logs nginx"""
+
+    # Connexion à la base de données MongoDB
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client['logs']
     collection = db['nginx']
+
+    # Liste des méthodes HTTP à analyser
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
+    # Affichage du nombre total de logs
     print(f"{collection.count_documents({})} logs")
+
+    # Affichage du nombre de logs pour chaque méthode HTTP de la liste
     print("Methods:")
     for method in methods:
         count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
+    # Affichage du nombre de logs avec méthode GET et au chemin /status
     print(collection.count_documents({"method": "GET", "path": "/status"}),
           "status check")
 
+    # Fermeture de la connexion à la base de données MongoDB
     client.close()
