18380bcb7b9dcf2dc7f6065b3cab2f08579252ec
lchenevi <leo.cheneviere@gmail.com>
2025-01-23 11:18:13 +0100

    corrected version of the task 0

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,5 +1,5 @@
 // Use displayMessage function to print a message to the console.
 const displayMessage = (message) => {
-	console.log(message);
-  };
-  module.exports = displayMessage;
+  console.log(message);
+};
+module.exports = displayMessage;
